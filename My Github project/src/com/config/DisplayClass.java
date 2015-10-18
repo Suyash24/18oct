5bfed149f4acd15a1be570f0e590a7d78474c513
suyash <suyashdesai20@gmail.com>
2015-10-18 19:06:33 +0530

    initial check in

---
+++ My Github project/src/com/config/DisplayClass.java
@@ -0,0 +1,10 @@
+package com.config;
+
+public class DisplayClass {
+	
+	public static void display()
+	{
+		System.out.println("suyash");
+	}
+
+}
---
+++ My Github project/src/com/mod/MyClass.java
@@ -0,0 +1,17 @@
+package com.mod;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.config.DisplayClass;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		WebDriver driver= new FirefoxDriver();
+		driver.get("https://www.google.co.in/");
+		DisplayClass.display();
+
+	}
+
+}
